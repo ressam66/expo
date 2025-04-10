5d051fc8f0c82a6937fca2a18c09da01b274e012
Phil Pluckthun <phil@kitten.sh>
2025-04-10 16:19:41 +0100

    expo-router: Rebuild

---
+++ packages/expo-router/build/rsc/globals.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    var __EXPO_RSC_RELOAD_LISTENERS__: undefined | (() => void)[];
+    var __EXPO_REFETCH_RSC__: undefined | (() => void);
+    var __EXPO_REFETCH_ROUTE__: undefined | (() => void);
+    var __EXPO_REFETCH_ROUTE_NO_CACHE__: undefined | (() => void);
+}
+export {};
+//# sourceMappingURL=globals.d.ts.map
